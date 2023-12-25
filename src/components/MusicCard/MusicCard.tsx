795721839ca361c660e149f95642b90591ce68c8
shadow <lightshadow416@gmail.com>
2023-12-25 15:17:31 +0100

    project checkpoint {1}

--- src/components/MusicCard/MusicCard.tsx
+++ src/components/MusicCard/MusicCard.tsx
@@ -1,4 +1,24 @@
 import "./musiccard.css";
+// assets
+import logo from "/icon-music.svg";
+// mui
+import { Card, CardContent, CardMedia, Box, Link } from "@mui/material";
+
 export default function MusicCard() {
-  return <>MusicCard</>;
+  return (
+    <>
+      <Card className="flex items-center justify-between px-6 !bg-VeryPaleBlue mt-[1rem]">
+        <Box className="flex items-center">
+          <CardMedia image={logo} component="img" />
+          <CardContent className="!py-[1rem]">
+            <p className="font-bold">Annual Plan</p>
+            <p>$59.99/year</p>
+          </CardContent>
+        </Box>
+        <Link underline="always" href="#" className="!text-BrightBlue">
+          Change
+        </Link>
+      </Card>
+    </>
+  );
 }
--- src/components/MyContainer/MyContainer.tsx
+++ src/components/MyContainer/MyContainer.tsx
@@ -1,13 +1,16 @@
 import "./mycontainer.css";
+// components
+import { MusicCard } from "../../components/";
 // mui
 import {
   Container,
   Card,
   CardActions,
   CardContent,
-  CardHeader,
   Typography,
   CardMedia,
+  Button,
+  Box,
 } from "@mui/material";
 
 // assets
@@ -17,24 +20,33 @@ export default function MyContainer() {
   return (
     <>
       <Container
-        className="border-2 border-black h-screen !flex items-center justify-center"
+        className="bg-gray-400 h-screen !flex items-center justify-center"
         maxWidth="xl"
       >
-        <Card className="border-2 border-red-500 w-[30rem] text-center">
-          <CardMedia
-            image={img}
-            component="img"
-            className="h-[15rem] rounded-t-3xl"
-          />
-          <CardContent>
-            <Typography variant="h4" className="!font-bold !my-[1rem]">
-              Order Summary
-            </Typography>
-            <Typography variant="h6">
-              You can now listen to millions of songs, audiobooks, and podcasts
-              on any device anywhere you like!
-            </Typography>
-          </CardContent>
+        <Card className="w-[30rem] text-center !rounded-3xl  pb-[1rem]">
+          <CardMedia image={img} component="img" className="h-[15rem]" />
+          <Box className="px-[2rem]">
+            <CardContent>
+              <Typography variant="h4" className="!font-bold !my-[1rem] ">
+                Order Summary
+              </Typography>
+              <Typography variant="h6">
+                You can now listen to millions of songs, audiobooks, and
+                podcasts on any device anywhere you like!
+              </Typography>
+              {/*  */}
+              <MusicCard />
+              {/*  */}
+            </CardContent>
+            <CardActions className="flex flex-col items-center space-y-[2rem]">
+              <Button className="!bg-BrightBlue w-full !py-[1rem] !text-white !rounded-xl">
+                Proceed to Payment
+              </Button>
+              <Button className="!text-DesaturatedBlue !font-bold">
+                Cancel Order
+              </Button>
+            </CardActions>
+          </Box>
         </Card>
       </Container>
     </>
